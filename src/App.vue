4e3c43284d05cc9373f1579a93095660a0082064
marikudo <king.marikudo@gmail.com>
2021-04-02 17:35:13 +0800

    image

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-<div class="container">
-  <img alt="Vue logo" src="https://lh4.googleusercontent.com/AofvVh2-hZK8CZighNjKWkQpdyAdEbPxTGD49mHJ0jhlniR3F1QjRbzc9Q6XpJVxB8LuGA_bsRHJZPxVWUci=w1366-h625" class="logo" />
-  <img alt="Vue logo" src="https://lh3.googleusercontent.com/fife/ABSRlIrdWq396ZJthlIchV-4YdRY4h7gkRU-lS0F21eP0CsYN_t4JAmaFQQCF_ndlrlvRjlP5EmErMeXrlbHwfsSbosiFgc_f2Y5IpTXRqa9bxvgxf2I5mpDoHIRpAKVGgRhHezo3ALk9ege0dntsfOthYRjfpeBDO5MTqTA5mgNutyyZSD8-0p8Nc_xvTLCpDB1hn9KqmwdYWNxbvnGZCKU54kNNLURr7VMnzNWjnsY_verr9puJd6hulg7A1ISxEhIrKYJ-9jwWffXzwSR97-JWsdolEQSLtVGXhNgtZvWCtXb9P_dDAZVcaw4f07CB5z7WiXNycH7TOkRpXY8Y9i5xi3Pah2iI5BXF_baqItaoJ5GGXrlByDVnoj0IbDth70e78R8YaORnHOTuTXfUURF99fU2opOQiR0eHEvZRlHcF538KlqODm9sUb9UnrL7MKhSPLbwMXgNinD2S1acRqoPZDwl9P-A3yxkOGJ_ujkihhKj277IHsxuwWa6SK08DF_T9fkEXoWEEt5V-NEKk_v0M0ZqzsLuMyEgiBlDUEMTa3Ti8BTpduWboIVInkNkvkyCIX1pVvlORU39yJTe0dIhCAUI6jMkM1mhC7XFLSK-wzyDCZaGVBYF4jrajYYOgE3PJTB92A92a3vPGGaSpgyJUqUgyJgufxEx6Mp3qdOrYehpL3NCqNZRNZI5ciNBGbQBtsaxUBkemUekgsU35NPUlmguguAx0M=w1366-h625-ft" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-</div>
+<HelloWorld msg="Hello Vue 3.0 + Vite" />>
 </template>
 
 <script>
